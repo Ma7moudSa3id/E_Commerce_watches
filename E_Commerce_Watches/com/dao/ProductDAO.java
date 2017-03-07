51b994248dece4060cb78b553151d0ab79e8afc3
tk <tk@tk-PC>
2017-03-07 19:46:46 +0200

    edited ProductDAO and ProductDTO

--- E_Commerce_Watches/com/dao/ProductDAO.java
+++ E_Commerce_Watches/com/dao/ProductDAO.java
@@ -6,15 +6,54 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import ecommerce.Utilities;
+import dto.ProductDTO;
 
 public class ProductDAO {
-	public static int getProductID(String name) throws SQLException, ClassNotFoundException {
-		Connection connection = Utilities.openConnection();
-		Statement statement = Utilities.createStatement(connection);
-		String sql = "select idProduct from product where prod_name = \"" + name + "\"";
-		ResultSet resultSet = statement.executeQuery(sql);
-		resultSet.next();
-		return resultSet.getInt("idProduct");
-	}
+
+    public static int getProductID(String name) throws SQLException, ClassNotFoundException {
+        Connection connection = Utilities.openConnection();
+        Statement statement = Utilities.createStatement(connection);
+        String sql = "select idProduct from product where prod_name = \"" + name + "\"";
+        ResultSet resultSet = statement.executeQuery(sql);
+        resultSet.next();
+        int productID = resultSet.getInt("idProduct");
+        connection.close();
+        statement.close();
+        resultSet.close();
+        return productID;
+    }
+
+    public static void addProduct(ProductDTO productDTO) throws SQLException, ClassNotFoundException {
+        String prod_name = productDTO.getName();
+        int prod_pricel = productDTO.getPrice();
+        int prod_amount = productDTO.getAmount();
+        String prod_category = productDTO.getCategory();
+        Connection connection = Utilities.openConnection();
+        Statement Statement = Utilities.createStatement(connection);
+        String sql = "insert into product (prod_name,prod_price,prod_amount,prod_category) values ("
+                + "\"" + prod_name + "\"" + "," + prod_pricel + "," + prod_amount + "," + "\"" + prod_category + "\"" + ")";
+        Statement.executeUpdate(sql);
+        connection.close();
+        Statement.close();
+
+    }
+
+    public static ProductDTO getProduct(int productID) throws SQLException, ClassNotFoundException {
+        ProductDTO productDTO = new ProductDTO();
+        Connection connection = Utilities.openConnection();
+        Statement statement = Utilities.createStatement(connection);
+        String sql = "SELECT prod_name , prod_price , prod_amount , prod_category FROM `e-commerce`.product where idProduct = " + productID;
+        ResultSet resultSet = statement.executeQuery(sql);
+        resultSet.next();
+        productDTO.setName(resultSet.getString("prod_name"));
+        productDTO.setPrice(resultSet.getInt("prod_price"));
+        productDTO.setAmount(resultSet.getInt("prod_amount"));
+        productDTO.setCategory(resultSet.getString("prod_category"));
+        connection.close();
+        statement.close();
+        resultSet.close();
+        return productDTO;
+
+    }
 
 }
---
+++ E_Commerce_Watches/com/dto/ProductDTO.java
@@ -0,0 +1,43 @@
+package dto;
+
+public class ProductDTO {
+
+    private String name;
+    private String category;
+
+    private int price;
+    private int amount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+}
