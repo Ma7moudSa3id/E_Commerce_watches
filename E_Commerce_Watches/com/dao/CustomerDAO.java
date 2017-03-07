4eb74a8f9b3ed4d71cad8308805fd5ecec7a7d28
mohamed <mohamed@lenovo>
2017-03-07 22:59:14 +0200

    added customerDTO & edited CustomerDAO

--- E_Commerce_Watches/com/dao/CustomerDAO.java
+++ E_Commerce_Watches/com/dao/CustomerDAO.java
@@ -16,4 +16,24 @@ public class CustomerDAO {
 		resultSet.next();
 		return resultSet.getInt("idCustomer");
 	}
+	 public static CustomerDTO showCustomerData(int customerID) throws SQLException, ClassNotFoundException {
+	        CustomerDTO customerDTO = new CustomerDTO();
+	        String sql = "select * from customer where idCustomer =" + customerID;
+	        Connection connection = Utilities.openConnection();
+	        Statement statement = Utilities.createStatement(connection);
+	        ResultSet resultSet = statement.executeQuery(sql);
+	        resultSet.next();
+	        customerDTO.setName(resultSet.getString("cst_name"));
+	        customerDTO.setAddress(resultSet.getString("cst_address"));
+	        customerDTO.setCreditlimit(resultSet.getInt("cst_creditlimit"));
+	        customerDTO.setJob(resultSet.getString("cst_job"));
+	        customerDTO.setMail(resultSet.getString("cst_mail"));
+	        customerDTO.setPass(resultSet.getString("cst_pass"));
+	        customerDTO.setPhone(resultSet.getString("cst_phone"));
+	        connection.close();
+	        statement.close();
+	        resultSet.close();
+	        return customerDTO;
+
+	    }
 }
---
+++ E_Commerce_Watches/com/dto/CustomerDTO.java
@@ -0,0 +1,70 @@
+package dto;
+
+public class CustomerDTO {
+
+    private String name;
+    private String pass;
+    private int creditlimit;
+    private String mail;
+    private String phone;
+    private String address;
+    private String job;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public int getCreditlimit() {
+        return creditlimit;
+    }
+
+    public void setCreditlimit(int creditlimit) {
+        this.creditlimit = creditlimit;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+    
+
+}
